694ff410b7e8030a9241fccef87c2e77fde80832
Serinho <Serinho@DESKTOP-RKRL7QF>
2019-11-13 19:46:59 +0100

    Pristup repozitorijumu

--- DizajnerskiObrasci/src/mvc/Application.java
+++ DizajnerskiObrasci/src/mvc/Application.java
@@ -3,7 +3,7 @@ package mvc;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Dobrodošli na vežbe iz predmeta Dizajnerski obrasci.");
+		System.out.println("Projekat zapocet.");
 
 	}
 
